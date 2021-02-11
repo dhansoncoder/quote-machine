8b1aacb713e5564e0097c79133ac27de27eeb10d
Adi <addisonhanson@MacBook-Pro-2.local>
2021-02-10 17:12:14 -0800

    donezo

--- app.js
+++ app.js
@@ -4,9 +4,21 @@ $(document).ready(function(){
         console.log('and Jquery is right')
         $(".words").slideUp()
 
-        var response= await fetch("https://quote-garden.herokuapp.com/api/v3/quotes/")
+        var response= await fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random")
+        
+
         var parsedResponse=await response.json()
-        console.log(parsedResponse)
+     /*   
+        var lengthoflist=parsedResponse.data.length
+        var randomnumber=Math.floor(Math.random()*lengthoflist)
+        */
+       console.log(parsedResponse.data[0].quoteText)
+       $("#quote-text").text(parsedResponse.data[0].quoteText)
+       $("#quote-name").text("- "+parsedResponse.data[0].quoteAuthor)
+        
+    
+        
+    
         $(".quote-container").animate({height:"5px"}).delay(200).animate({height:"100%"})
         $(".words").slideDown(1000)
 
